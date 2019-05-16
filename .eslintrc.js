b688438dd87f9b0d3a2b8e2595efbe3d54d22e5d
DylanMerigaud <dylanmerigaud@gmail.com>
2019-05-16 11:09:45 +0200

    CI use base64 to encode env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   rules: {
     semi: ["error", "never"],
     "comma-dangle": ["error", "always"],
     "prefer-arrow-callback": "error",
-  },
-}
+    quotes: ["error", "single"]
+  }
+};
